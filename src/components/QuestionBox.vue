7563d95708c0da69bbd7971b92f6d828b9e8dd49
mhdruslan.ashaari <mhdruslan.ashaari@myeg.com.my>
2021-06-28 08:15:42 +0800

    add header and questionbox component

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="question-box-container">
+		<b-jumbotron lead="Bootstrap 4 Components for Vue.js 2">
+			<template slot="lead">
+				Question?
+			</template>
+
+			<hr class="my-4" />
+
+			<p>
+				List of answers
+			</p>
+
+			<b-button variant="primary" href="#">Submit</b-button>
+			<b-button variant="success" href="#">Next</b-button>
+		</b-jumbotron>
+	</div>
+</template>
